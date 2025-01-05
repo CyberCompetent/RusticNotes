f7e5c4050409e21c73263b724a6fb4b4e720d821
Nathan van Niekerk <nathvn3@protonmail.com>
2025-01-05 14:21:15 +0000

    add untracked files

--- src/app/components/templates/SmallButton.tsx
+++ src/app/components/templates/SmallButton.tsx
@@ -4,13 +4,17 @@ interface SmallButtonProps {
   id: string;
   svg?: string;  // Optional svg prop
   children: React.ReactNode;
+  onClick?: () => void; // Optional onClick handler
+  isActive?: boolean; // Optional isActive prop for conditional styling
+  className?: string; // Optional className for custom styling
 }
 
-const SmallButton: React.FC<SmallButtonProps> = ({ id, svg, children }) => {
+const SmallButton: React.FC<SmallButtonProps> = ({ id, svg, children, onClick, isActive = false, className = '' }) => {
   return (
     <button
       id={id}
-      className="bg-gray-800 text-white py-2 px-4 rounded-lg flex items-center gap-2"
+      className={`text-white py-2 px-2 rounded-lg flex items-center gap-2 ${isActive ? 'bg-gray-900 hover:bg-black' : 'bg-gray-700 hover:bg-gray-800'} ${className}`}
+      onClick={onClick}
     >
       {svg && <span className="material-icons text-xl">{svg}</span>} {/* Conditionally render the SVG if provided */}
       {children}
